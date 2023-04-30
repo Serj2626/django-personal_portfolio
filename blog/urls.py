8656b0e90b49718010663c2cc9a041ef9c8ccc30
Sergey Boytsov <serj2626@mail.ru>
2023-04-30 15:12:56 +0300

    create template detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
-
-
+    path('<int:blog_id>/', views.all_detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,11 @@ from .models import Blog
 
 
 def all_blogs(request):
+    '''function to display all blogs'''
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+
+def all_detail(request, blog_id):
+    '''function to display blog by id'''
+    return render(request, 'blog/detail.html', {'id': blog_id})
