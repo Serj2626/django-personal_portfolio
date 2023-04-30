2d9824c192c38fc52a8b30f679e84c99de33010e
Sergey Boytsov <serj2626@mail.ru>
2023-04-30 17:50:20 +0300

    update style in custom.css

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class Project(models.Model):
+    '''project description model'''
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
 
+    def __str__(self):
+        return self.title
